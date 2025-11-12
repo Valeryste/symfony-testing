755ac2b81b4fa631d5f4b37f1aa8be791dd9c93a
Valeryste <valera.stelmashock@yandex.ru>
2025-11-12 09:30:52 +0300

    setting swagger api for api

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
