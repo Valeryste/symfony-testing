a859fb7abc979b48f0ff17bb8f2c0b1ae2009bda
Valeryste <valera.stelmashock@yandex.ru>
2026-01-06 12:08:57 +0300

    created check to lock the user

--- laravel/routes/api/authRoutes.php
+++ laravel/routes/api/authRoutes.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthenticationController::class)
     ->group(function () {
-        Route::post('login', 'login');
-        Route::post('register', 'register');
-        Route::post('user', 'user')->middleware('auth:sanctum');
-        Route::post('logout', 'logout')->middleware('auth:sanctum');
+        Route::post('login', 'login')->name('login');
+        Route::post('register', 'register')->name('register');
+        Route::post('user', 'user')->middleware(['auth:sanctum', 'user.active'])->name('user');
+        Route::post('logout', 'logout')->middleware(['auth:sanctum', 'user.active'])->name('logout');
     });
